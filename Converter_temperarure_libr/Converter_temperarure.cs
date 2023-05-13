4a6116a308e06fa54c06fe5218438581897e8dd1
Vlad Dvorskiy <dvorskiy6@gmail.com>
2023-05-14 00:01:51 +0300

    1

---
+++ Converter_temperarure_libr/Converter_temperarure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter_temperarure_libr
+{
+    class Converter_temperarure
+    {
+        public void CelsiusToFahrenheit()
+        {
+            //Console.Write($"Введино цельсия: {VALUE}");
+            int Celsius = 50; /*int.Parse(VALUE);*/
+            Celsius = ((Celsius - 32) * 5) / 9;
+            Console.Write($"Fahrenheit To Celsius - {Celsius}");
+            object test = new object();
+        }
+        public void FahrenheitToCelsius()
+        {
+            //Console.WriteLine("Введите: ");
+            //double Celsius = double.Parse(Console.ReadLine());
+            int Fahrenheit = 10; /*int.Parse(value);*/
+            Fahrenheit = ((Fahrenheit * 9) / 5) + 32;
+            Console.Write($"Celsius To Fahrenheit - {Fahrenheit}");
+            object test = new object();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Reflection_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Reflection_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var libraryPas = @"C:\Users\Мой друг\source\repos\Converter_temperarure_libr\Converter_temperarure_libr\bin\Debug\Converter_temperarure_libr.dll";
+            Assembly assembly = Assembly.LoadFrom(libraryPas);
+            var printerClass = assembly.GetTypes().First(t => t.FullName == TYPE_NAME);
+            foreach (Type type in assembly.GetTypes())
+            {
+                Console.WriteLine($"Full name: {type.FullName}");
+                Console.WriteLine($"Is visible: {type.IsVisible}");
+                Console.WriteLine($"Base type: {type.BaseType}");
+                Console.WriteLine($"Module: {type.Module}");
+            }
+            Console.WriteLine();
+            var instanse = Activator.CreateInstance(printerClass);
+            MethodInfo printMethod = printerClass.GetMethod(TYPE_Celsius);
+            printMethod.Invoke(instanse,null);
+            Console.WriteLine();
+            MethodInfo print = printerClass.GetMethod(TYPE_FahrenheitToCelsius);
+            print.Invoke(instanse,null);
+            Console.ReadKey();
+        }
+        static string TYPE_NAME = "Converter_temperarure_libr.Converter_temperarure";
+        static string TYPE_Celsius = "CelsiusToFahrenheit";
+        static string TYPE_FahrenheitToCelsius = "FahrenheitToCelsius";
+    }
+}
